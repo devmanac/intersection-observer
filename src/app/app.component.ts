61247afffe7135b30edf2e540d40953253de49e1
devmanac <devmanac@gmail.com>
2022-05-29 12:28:50 +0200

    basic intersection observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    const cards = document.querySelectorAll(".card");
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting);
+      })
+      console.log(entries);
+    })
+    cards.forEach(card => observer.observe(card));
+  }
   title = 'intersection-observer';
+  
 }
