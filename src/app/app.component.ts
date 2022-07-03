045dccbf30b1114a5a5f7d6781cfec4f6607cf9d
devmanac <devmanac@gmail.com>
2022-07-03 19:29:22 +0200

    added new cards for infinite scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit{
   ngOnInit(): void {
@@ -14,8 +15,48 @@ export class AppComponent implements OnInit{
       })
       console.log(entries);
     })
-    cards.forEach(card => observer.observe(card));
+
+    cards.forEach(card => {
+      observer.observe(card);
+    })
+    
+
+    // Observes last card in container and adds new card
+    const lastCardObserver = new IntersectionObserver(entries => {
+      const lastCard = entries[0];
+      if (!lastCard.isIntersecting ) {
+        return;
+      }
+      loadNewCards();
+      lastCardObserver.unobserve(lastCard.target);
+      const newLastCard =  document.querySelector(".card:last-child");
+      if (!!newLastCard) {
+        lastCardObserver.observe(newLastCard);
+      }
+    
+      
+    })
+
+    const cardContainer = document.querySelector(".card-container");
+    function loadNewCards(): void {
+      for (let i=0; i<10; i++) {
+        const card = document.createElement("div");
+        card.textContent = "New Card";
+        card.classList.add("card");
+        observer.observe(card);
+        cardContainer?.append(card);
+      }
+    }
+
+    const lastCardForObserver = document.querySelector(".card:last-child");
+    if (!!lastCardForObserver) {
+      lastCardObserver.observe(lastCardForObserver);
+    }
+
+    
   }
   title = 'intersection-observer';
+
+
   
 }
